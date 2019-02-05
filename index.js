40985df0fe9a4679b6cbea8e42c41a05a5e12243
Pamela Frieze <pfrieze@gmail.com>
2019-02-05 18:52:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const badges = ['Ada', 'Brendan', 'Ali'];
 
 function printBadges(badges) {
   for (let i = 0; i < badges.length; i++) {
+    
   console.log('Welcome ${badges[i]}! You are employee ${badges.length.')  
   }
 }
